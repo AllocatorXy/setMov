d53c9234857205b5669acf5db55c8d1eae0f0482
AllocatorXy <misaka20001@vip.qq.com>
2016-09-16 19:47:34 +0800

    -

---
+++ js/js.js
@@ -0,0 +1,54 @@
+window.onload = function () 
+{
+	var oDiv = document.getElementsByTagName('div')[0];
+
+	oDiv.onmouseover = function () 
+	{
+		setMov(this,'height',500,'1');
+		setMov(this,'width',500,'2');
+		setMov(this,'font-size',16,'3');
+	};
+	oDiv.onmouseout = function () 
+	{
+		setMov(this,'height',300,'1');
+		setMov(this,'width',300,'2');
+		setMov(this,'font-size',70,'3');
+	};
+};
+
+
+function setMov(obj, attr, iTar, timer) 
+{
+	clearInterval(obj[timer]);
+
+	obj[timer] = setInterval(function () 
+	{
+		var realAttr = parseInt(getStyle(obj,attr));
+		var speed = (iTar-realAttr)/10;
+		speed = speed>0?Math.ceil(speed):Math.floor(speed);
+
+		if (realAttr == iTar) 
+		{
+			clearInterval(obj[timer]);
+		}
+		else
+		{
+			obj.style[attr] = realAttr+speed+'px';
+		}
+	}, 15);
+}
+function getStyle(obj,name) 
+{
+	
+	if (obj.currentStyle) 
+	{
+		//IE低版本
+		return obj.currentStyle[name];
+	}
+	else 
+	{
+		//FF等浏览器
+		return getComputedStyle(obj,null)[name]; 
+		//getComputedStyle函数中，第二个参数无用，任意设置
+	}
+}
