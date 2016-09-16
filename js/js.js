4d72b2ce79cb4611cc39973edb7820cc898d4f41
AllocatorXy <misaka20001@vip.qq.com>
2016-09-16 19:50:51 +0800

    -

--- js/js.js
+++ js/js.js
@@ -1,38 +1,4 @@
-function setMov(obj, attr, iTar, timer) 
-{
-	clearInterval(obj[timer]);
 
-	obj[timer] = setInterval(function () 
-	{
-		var realAttr = parseInt(getStyle(obj,attr));
-		var speed = (iTar-realAttr)/10;
-		speed = speed>0?Math.ceil(speed):Math.floor(speed);
-
-		if (realAttr == iTar) 
-		{
-			clearInterval(obj[timer]);
-		}
-		else
-		{
-			obj.style[attr] = realAttr+speed+'px';
-		}
-	}, 15);
-}
-function getStyle(obj,name) 
-{
-	
-	if (obj.currentStyle) 
-	{
-		//IE低版本
-		return obj.currentStyle[name];
-	}
-	else 
-	{
-		//FF等浏览器
-		return getComputedStyle(obj,null)[name]; 
-		//getComputedStyle函数中，第二个参数无用，任意设置
-	}
-}
 window.onload = function () 
 {
 	var oDiv = document.getElementsByTagName('div')[0];
@@ -49,4 +15,39 @@ window.onload = function ()
 		setMov(this,'width',300,'2');
 		setMov(this,'font-size',70,'3');
 	};
+	function setMov(obj, attr, iTar, timer) 
+	{
+		clearInterval(obj[timer]);
+
+		obj[timer] = setInterval(function () 
+		{
+			var realAttr = parseInt(getStyle(obj,attr));
+			var speed = (iTar-realAttr)/10;
+			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+
+			if (realAttr == iTar) 
+			{
+				clearInterval(obj[timer]);
+			}
+			else
+			{
+				obj.style[attr] = realAttr+speed+'px';
+			}
+		}, 15);
+	}
+	function getStyle(obj,name) 
+	{
+		
+		if (obj.currentStyle) 
+		{
+			//IE低版本
+			return obj.currentStyle[name];
+		}
+		else 
+		{
+			//FF等浏览器
+			return getComputedStyle(obj,null)[name]; 
+			//getComputedStyle函数中，第二个参数无用，任意设置
+		}
+	}
 };
